9205ebc37390685b566399b3d11c86809980b8c3
robert fine <robertfine4631@icloud.com>
2015-06-23 13:25:20 -0400

    attempting to set up, but still can't quite figure out this out

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,35 @@
 ;(function (){
 
-	'use strict';
+  'use strict';
 
+  angular.module('counterModule', [])
 
+  .controller('counterController', ['$scope', function ($scope){
+//creating an array to count the likes
+    $scope.allLikes = [];
+
+//creating constructor for the like
+    var allLikes = function (likeNum) {
+
+    };
+//Counter of array
+   $scope.numberOf = function () {
+      var likes = _.filter($scope.allLikes, function (x){
+        return x.length;
+
+      });
+      return numberOf.length;
+    };
+
+    
+
+//Push to button
+    $scope.addLike = function (lke) {
+      var likenum = new allLikes(lke.like);
+      $scope.allLikes.push(lke);
+
+    };
+
+    }
+  ]);
 }());
