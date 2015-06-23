30cd452c7dc30fb077106c03c8e3c963607130aa
robert fine <robertfine4631@icloud.com>
2015-06-23 13:40:35 -0400

    added constructor

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,15 @@
 
   .controller('counterController', ['$scope', function ($scope){
 
+//Created array for likes to be counted in
+    $scope.allLikes = [];
+//Create constructor for each like
+    var like = function (addlike) {
+      this.like = addlike;
+    }
+
+
+
 
     }
   ]);
